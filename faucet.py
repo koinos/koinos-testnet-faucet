2420f5e61ee07eee70bba94d7c6fb70aa520eec3
Nathaniel Caldwell <nbcwell@gmail.com>
2021-06-01 15:34:13 -0400

    Create transactions

--- faucet.py
+++ faucet.py
@@ -1,12 +1,18 @@
+import re
 import dbm
 import json
 import argparse
+import subprocess
 from datetime import datetime
 
 import yaml
+import base58
 
 from bottle import run, post
 from bottle import default_app, request, response
+
+app = default_app()
+re_address = re.compile("0x[0-9a-fA-F]{40}$")
 
 class Blockchain:
     def __init__(self):
@@ -16,6 +22,10 @@ class Blockchain:
         return
 
     def transfer(self, address, amount):
+        transaction = create_transaction(address, amount)
+        call = [app.config["signing_tool_bin"], "-w", "-k", app.config["private_key_file"]]
+        rpc_call = subprocess.check_output(call)
+        # TODO: Make actual rpc call here
         return
 
     def update_balance(self):
@@ -23,7 +33,28 @@ class Blockchain:
         self.balance = 10000000000 #(1000.0000000)
         return
 
-app = default_app()
+def create_transaction(to_address, amount):
+    args = create_args(app.config["wallet_address"], to_address, amount)
+    transaction = {'id': 'z11', 'active_data': {'resource_limit': 0,
+    'nonce': 0, 'operations': [{'type': 'koinos::protocol::call_contract_operation',
+    'value': {'contract_id': app.config["contract_id"], 'entry_point': app.config["entry_point"], 'args': args, 'extensions': {}}}]},
+    'passive_data': {}, 'signature_data': ''}
+    return transaction
+
+def create_args(from_address, to_address, amount):
+    from_bytes = address_to_bytes(from_address)
+    to_bytes = address_to_bytes(to_address)
+    amt_bytes = amount.to_bytes(8, 'big')
+    return to_base58(from_bytes + to_bytes + amt_bytes)
+
+def to_base58(b):
+    s = base58.b58encode(b)
+    return "z" + "".join(chr(x) for x in s)
+
+# address in form 0x...
+def address_to_bytes(address):
+    print(address)
+    return b'\x14' + bytes.fromhex(address[2:])
 
 def check_key(name, password):
     return True # TODO: Add authentication
@@ -73,6 +104,11 @@ def request_koin():
         response.status = 400
         return json.dumps({"message": "Input error."})
 
+    # Ensure the address has a valid format
+    if re_address.match(address) is None:
+        response.status = 400
+        return json.dumps({"message": "Invalid address format."})
+
     # Check the identifier against the database
     id_result = check_identifier(id)
     if not id_result[0]:
@@ -82,7 +118,7 @@ def request_koin():
 
     # Execute the payout
     amount = pay_address(address)
-    s_amount = "{:8f}".format(amount / 10000000.0)
+    s_amount = "{:7f}".format(amount / 10000000.0)
 
     response.status = 202
     response.headers['Content-Type'] = 'application/json'
